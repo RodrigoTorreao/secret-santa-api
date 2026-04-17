7f4fe97c8aa2b35c6fa3d3f160173b6f7f0cfa75
Rodrigo <rodrigo.torreao@gmail.com>
2026-04-17 14:24:17 -0300

    feat: auth-module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+    imports: [
+        UsersModule,
+        JwtModule.register({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+            expiresIn: '1h',
+        },
+        }),
+    ],
+    providers: [AuthService],
+    exports: [AuthService],
+})
 export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import * as bcrypt from 'bcrypt'
+
+import { UsersService } from '../users/users.service'
+import { JwtService } from '@nestjs/jwt'
+
+@Injectable()
+export class AuthService{
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService 
+    ){}
+
+    async validateUser(email: string, password: string) {
+        const user = await this.usersService.findByEmail(email)
+        if(!user){
+            throw new UnauthorizedException('Invalid Credentials')
+        }
+        const passwordValid = await bcrypt.compare(
+            password,
+            user.passwordHash,
+        )
+        if(!passwordValid){
+            throw new UnauthorizedException('Invalid Credentials')
+        }
+        return this.sanitize(user)
+
+    }
+
+    async login(email: string, password:string){
+        const user = await this.validateUser(email, password)
+        const payload = {
+            sub: user.id,
+            email: user.email,
+        }
+
+        const accessToken = await this.jwtService.signAsync(payload)
+
+        return{
+            accessToken,
+            user
+        }
+    }
+
+    sanitize(user:any){
+        const {passordHash, ...safeUser} = user
+        return safeUser
+    }
+
+}
